200be61dd3eab5fc84db314649994d3f136a486d
David L. Enock <118814205+davunju@users.noreply.github.com>
2024-03-04 15:06:00 +0300

    updates

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const dayDisplay = document.querySelector("#dys");
 const button = document.getElementById("btn");
 const errors = document.querySelectorAll("#error");
 const labels = document.querySelectorAll("label");
+const errorDay = document.querySelector(".error-day");
+const errorMonth = document.querySelector(".error-month");
+const errorYear = document.querySelector(".error-year");
+const form = document.querySelector("form");
 
 const isRequired = (value) => (value === "" ? false : true);
 
@@ -34,6 +38,50 @@ function getYearsOld() {
   }
 }
 
+const dayValid = () => {
+  if (day.value > 31) {
+    errorDay.textContent = "Must be a valid day";
+  } else if (!isRequired(day.value)) {
+    errorDay.textContent = "This field is required";
+  } else {
+    errorDay.textContent = "";
+  }
+};
+
+const monthValid = () => {
+  if (month.value > 12) {
+    errorMonth.textContent = "Must be a valid month";
+  } else if (!isRequired(month.value)) {
+    errorMonth.textContent = "This field is required";
+  } else {
+    errorMonth.textContent = "";
+  }
+};
+
+const yearValid = () => {
+  if (year.value > new Date().getFullYear()) {
+    errorYear.textContent = "Must be in the past";
+  } else if (!isRequired(year.value)) {
+    errorYear.textContent = "This field is required";
+  } else {
+    errorMonth.textContent = "";
+  }
+};
+
+form.addEventListener("input", (e) => {
+  switch (e.target.id) {
+    case "day":
+      dayValid();
+      break;
+    case "month":
+      monthValid();
+      break;
+    case "year":
+      yearValid();
+      break;
+  }
+});
+
 errors.forEach((error) => {
   button.addEventListener("click", (e) => {
     e.preventDefault();
@@ -41,10 +89,9 @@ errors.forEach((error) => {
       mob = month.value,
       yob = year.value;
     if (!isRequired(dob) || !isRequired(mob) || !isRequired(yob)) {
-      error.textContent = "This field is required";
-      labels.forEach((label) => {
-        label.style.color = "hsl(0, 100%, 67%)";
-      });
+      dayValid();
+      monthValid();
+      yearValid();
     } else {
       error.textContent = "";
       labels.forEach((label) => {
